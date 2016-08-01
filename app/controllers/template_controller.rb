2c6ec191e6c108bcc4e4079771793092e80c31b9
Davis <davis@DavisdeMacBook-Air.local>
2016-08-01 23:28:38 +0800

    build layout

---
+++ app/controllers/template_controller.rb
@@ -0,0 +1,12 @@
+class TemplateController < ApplicationController
+  before_filter :authenticate_user!
+
+  def index
+
+  end
+
+  def template
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users
+
+  root :to => "home#index"
+
+  get '/dashboard' => 'templates#index'
 end
